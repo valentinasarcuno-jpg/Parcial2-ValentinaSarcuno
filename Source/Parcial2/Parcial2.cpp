f03e928db28e6596e3fad0ea67ca1acafc6e0a25
valentinasarcuno-jpg <valentinasarcuno@upc.edu.ar>
2026-09-15 19:25:08 -0300

    Cambios en el repositorio

---
+++ Source/Parcial2/Parcial2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Parcial2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Parcial2, "Parcial2" );
